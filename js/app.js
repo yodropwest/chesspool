6dd8c65144869f7a98d1f5c27d2590e02982fe4b
yodropwest <e.sumkin@meta.studio>
2022-04-04 09:20:06 +0700

    feat: added yandex map

--- js/app.js
+++ js/app.js
@@ -5,32 +5,16 @@ var swiper = new Swiper(".mySwiper", {
 
 ymaps.ready(init);
 
-function init () {
+function init() {
     var myMap = new ymaps.Map("map", {
-            center: [54.83, 37.11],
-            zoom: 5
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
-        myPlacemark = new ymaps.Placemark([55.907228, 31.260503], {
-            // Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
-            balloonContentHeader: "Балун метки",
-            balloonContentBody: "Содержимое <em>балуна</em> метки",
-            balloonContentFooter: "Подвал",
-            hintContent: "Хинт метки"
+            center: [55.758154, 37.552831],
+            zoom: 14,
+            controls: []
+        },),
+        myPlacemark = new ymaps.Placemark([55.758154, 37.552831], {
+            balloonContentBody: "Шмитовский проезд, 9/5",
         });
 
     myMap.geoObjects.add(myPlacemark);
 
-    // Открываем балун на карте (без привязки к геообъекту).
-    myMap.balloon.open([51.85, 38.37], "Содержимое балуна", {
-        // Опция: не показываем кнопку закрытия.
-        closeButton: false
-    });
-
-    // Показываем хинт на карте (без привязки к геообъекту).
-    myMap.hint.open(myMap.getCenter(), "Одинокий хинт без метки", {
-        // Опция: задержка перед открытием.
-        openTimeout: 1500
-    });
 }
